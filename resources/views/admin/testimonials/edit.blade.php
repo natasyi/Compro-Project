25ec6b340a899cb0f62679e0c83108b52e451d2d
natasyi <tasya.arifah22@gmail.com>
2025-02-04 20:26:08 +0700

    edit testimonials

--- resources/views/admin/testimonials/edit.blade.php
+++ resources/views/admin/testimonials/edit.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden p-10 shadow-sm sm:rounded-lg">
                 
-                <form method="POST" action=" " enctype="multipart/form-data">
+                <form method="POST" action="{{ route('admin.testimonials.update', $testimonial) }}" enctype="multipart/form-data">
 
 
                     <div class="mt-4">
